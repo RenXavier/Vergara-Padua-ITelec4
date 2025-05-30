607c79ede6932d9eb49ddfbc6b0a846d00e3f9bf
Yuekasha <asuncionarvie7@gmail.com>
2025-05-30 10:23:00 +0800

    done my part

---
+++ app/Http/Controllers/employeecontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Fascades\DB;
+use Response;
+use Illuminate\Http\Request;
+use App\Models\employee;
+
+class employeecontroller extends Controller
+{
+    public function index()
+    {   
+        $employees = employee::get();
+
+        return view ('employee.index',compact('employees'));
+    }
+        public function create()
+    {
+        return view ('employee.create');
+    }
+       public function store(Request $request){
+        $request->validate([
+
+        ]);
+       employee::create($request->all());
+       return view ('employee.create');
+    }
+
+
+        
+            // employee::findOrFail($id)->update($request->all());
+            // return redirect ()->back()->with('status','Employee Updated Successfully!');
+            // }
+
+}
